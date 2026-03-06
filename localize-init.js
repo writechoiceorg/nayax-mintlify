a8084f217c108719470db5e46132562ed3d98b11
Gabriel <gabrielraeder@outlook.com.br>
2026-03-06 15:16:45 -0300

    save localize script

---
+++ localize-init.js
@@ -0,0 +1,11 @@
+!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","getSourceLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
+
+var script = document.createElement('script');
+script.src = 'https://global.localizecdn.com/localize.js';
+script.onload = function() {
+  Localize.initialize({
+    key: 'pZ1G7g0CPwJGQ',
+    rememberLanguage: true,
+  });
+};
+document.head.appendChild(script);
